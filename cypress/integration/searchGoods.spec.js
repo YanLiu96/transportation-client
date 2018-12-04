9ac55585fd0e4d33b380fbcc2bf05c943194f9a2
Yan Liu <278899085@qq.com>
2018-12-04 22:34:17 +0000

    allow a good to be deleted test

--- cypress/integration/searchGoods.spec.js
+++ cypress/integration/searchGoods.spec.js
@@ -1,13 +1,13 @@
-describe("Manage Donations page", () => {
+describe("Manage Goods page", () => {
 
   beforeEach(() => {
     // Delete all donations in the API's datastore
-    cy.request('https://express-transportation.herokuapp.com/goods')
+    cy.request('https://express-transportation.herokuapp.com/goods/')
       .its('body')
       .then( (goods) => {
         goods.forEach( (element) => {
           cy.request('DELETE',
-            'https://express-transportation.herokuapp.com/goods' + element._id)
+            'https://express-transportation.herokuapp.com/goods/' + element._id)
         });
       })
 
@@ -16,8 +16,21 @@ describe("Manage Donations page", () => {
       .then((goods) => {
         goods.forEach((good) => {
           cy.request('POST',
-            'https://express-transportation.herokuapp.com/goods', good )
+            'https://express-transportation.herokuapp.com/goods/', good )
         })
       })
+    cy.visit("/");
+    // Click Manage Donations navbar link
+    cy.get('.navbar-nav:nth-child(1)').find('.nav-item:nth-child(2)').click()
   });
+
+  it("allows a good to be deleted", () => {
+    cy.get('tbody').find('tr').should('have.length', 4)
+    // Click trash/delete link of 3rd donation in list
+    cy.get('tbody').find('tr:nth-child(2)').find('td:nth-child(10)').click()
+    // Click confirmation button
+    cy.get('button').contains('Delete').click()
+    cy.get('tbody').find('tr').should('have.length', 3)
+  });
+
 })
